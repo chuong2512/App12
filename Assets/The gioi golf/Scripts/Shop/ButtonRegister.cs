ac7244c3aa85b76558d3af89968b13baa744de83
chuong_gamer <hieutg244@gmail.com>
2023-07-05 21:49:59 +0700

    update app

---
+++ Assets/The gioi golf/Scripts/Shop/ButtonRegister.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonRegister : MonoBehaviour
+{
+    public int number;
+    public TextMeshProUGUI TextMeshProUgui;
+    public Button button;
+
+    void OnValidate()
+    {
+        TextMeshProUgui = GetComponentInChildren<TextMeshProUGUI>();
+        button = GetComponentInChildren<Button>();
+    }
+//////////////////////
+/// <summary>
+/// //////////
+/// </summary>
+    private void SetTextDay()
+    {
+        switch (number)
+        {//////////////////////
+            case 1:
+                TextMeshProUgui.SetText("Gia hạn thêm 1 ngày");
+                break;
+            case 2://////////////////////
+                TextMeshProUgui.SetText("Gia hạn thêm 7 ngày");
+                break;
+            case 3:
+                TextMeshProUgui.SetText("Gia hạn thêm 30 ngày");
+                break;
+            case 4:
+                TextMeshProUgui.SetText("Gia hạn thêm 90 ngày");
+                break;
+            case 5:
+                TextMeshProUgui.SetText("Gia hạn thêm 180 ngày");
+                break;
+        }
+    }//////////////////////
+
+    void Start()
+    {
+        button.onClick.AddListener(Choose);
+
+        SetTextDay();
+    }
+
+    private void Choose()
+    {
+        RegisterManager.Instance.OnPressDown(number);
+    }
+}
